cd3344be4ec1ea927e30360cb5d7c4a9ac644019
devkgn88 <devkgn88@gmail.com>
2024-09-10 14:40:47 +0900

    [김가남] 클래스 나누기 설명

---
+++ lec04_control/src/com/gn/test/Practice.java
@@ -0,0 +1,16 @@
+package com.gn.test;
+
+// 여러 기능을 담고 있는 클래스
+// 하나의 기능당 하나의 메소드를 생성
+public class Practice {
+	
+	// 1번 기능 : 인사말 출력
+	public void practice01() {
+		System.out.println("안녕하세요!! 반가워요~");
+	}
+	
+	// 2번 기능 : 두 숫자의 합계산
+	public void practice02() {
+		System.out.println("1 + 2 = "+(1+2));
+	}
+}
---
+++ lec04_control/src/com/gn/test/Run.java
@@ -0,0 +1,13 @@
+package com.gn.test;
+
+// 메인 메소드를 가지고 있는 클래스
+public class Run {
+	public static void main(String[] args) {
+		// Practice 클래스의 기능 호출
+		Practice practice = new Practice();
+		// 1번 기능 호출
+		practice.practice01();
+		// 2번 기능 호출
+		practice.practice02();
+	}
+}
