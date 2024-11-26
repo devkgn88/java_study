3b0026121b1583f9b45bcf8bd9c87fa32e21a66a
devkgn88 <devkgn88@gmail.com>
2024-11-26 23:43:15 +0900

    [kgn] 코드 일부 수정

--- lec03_operator/src/com/gn/practice/Practice07.java
+++ lec03_operator/src/com/gn/practice/Practice07.java
@@ -6,13 +6,18 @@ public class Practice07 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("첫번째 : ");
-		int num1 = sc.nextInt();
-		System.out.print("두번째 : ");
-		int num2 = sc.nextInt();
 		
-		int min = (num1 < num2) ? num1 : num2;
 		
-		System.out.println("두 수 중에서 작은 수는? "+min);
+//		System.out.print("첫번째 : ");
+//		int num1 = sc.nextInt();
+//		System.out.print("두번째 : ");
+//		int num2 = sc.nextInt();
+//		System.out.print("세번째 : ");
+//		int num3 = sc.nextInt();
+//				
+//		int max = (num1 < num2) ? num2 : num1;
+//		max = (num3 < max) ? max : num3;
+//		
+//		System.out.println("세 수 중에서 큰 수는? "+max);
 	}
 }	
