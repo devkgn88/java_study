c605b265583f6091f91b8821e9ebd1530090e2a2
devkgn88 <devkgn88@gmail.com>
2024-11-27 23:25:03 +0900

    [kgn] for문 실습

---
+++ lec04_control/src/com/gn/study/LoopStmt.java
@@ -0,0 +1,10 @@
+package com.gn.study;
+
+public class LoopStmt {
+	public static void main(String[] args) {
+		// 1. for문
+		for (int i = 1; i <= 10; i++) {
+		    System.out.println(i);
+		}
+	}
+}
