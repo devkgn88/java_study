bbe6ac67d484de20a0d8c6349deae7314ec24173
devkgn88 <devkgn88@gmail.com>
2024-11-27 23:37:44 +0900

    [kgn] 중첩 반복문, 별찍기

--- lec04_control/src/com/gn/study/LoopStmt.java
+++ lec04_control/src/com/gn/study/LoopStmt.java
@@ -44,5 +44,21 @@ public class LoopStmt {
 		}
 
 		System.out.println("최종 count 값: " + count);
+		
+		for(int i = 1 ; i <= 2 ; i++) {
+			System.out.println("A");
+			for(int j = 1 ; j <= 3 ; j++) {
+				System.out.println("B");
+			}
+			System.out.println("C");
+		}
+		
+		System.out.println("===== 별찍기 ======");
+		for(int i = 0 ; i < 2 ; i++){
+			for(int j = 0 ; j < 4 ; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
