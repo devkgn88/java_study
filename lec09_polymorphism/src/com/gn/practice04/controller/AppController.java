f2c6f3251d9e7d6a0d32222d3574740aeeae084c
devkgn88 <devkgn88@gmail.com>
2024-12-08 17:37:07 +0900

    [kgn] 추상 실습

---
+++ lec09_polymorphism/src/com/gn/practice04/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.gn.practice04.controller;
+
+import com.gn.practice04.model.vo.AppFeatures;
+import com.gn.practice04.model.vo.LoginMenu;
+
+public class AppController {
+	public void startMyApp() {
+		AppFeatures login = new LoginMenu();
+		login.display();
+		login.input();
+		login.close();
+	}
+}
