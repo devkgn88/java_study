bd2394f22c427b924cb38d321ea6a4a843209caf
devkgn88 <devkgn88@gmail.com>
2024-12-11 20:59:28 +0900

    [kgn] HashSet 요소 추가

--- lec11_collection/src/com/gn/study/controller/Run.java
+++ lec11_collection/src/com/gn/study/controller/Run.java
@@ -2,7 +2,9 @@ package com.gn.study.controller;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.gn.study.model.vo.Student;
 
@@ -41,10 +43,30 @@ public class Run {
 		studentList.add(new Student("이영희",30));
 		studentList.add(new Student("홍길동",40));
 		
-//		Collections.sort(studentList);
-		Collections.sort(studentList, new Student());
+		Collections.sort(studentList);
+//		Collections.sort(studentList, new Student());
 
 		System.out.println(studentList);
+		
+		System.out.println("=============================");
+		// HashSet요소 추가
+		Set<String> city = new HashSet<String>();
+		city.add("서울");
+		city.add("부산");
+		System.out.println(city);
+		// 중복된 정보 추가 시도 후 출력
+		city.add("서울");
+		System.out.println(city);
+		
+		Set<Student> set1 = new HashSet<Student>();
+		set1.add(new Student("김철수",15));
+		set1.add(new Student("홍길동",21));
+		set1.add(new Student("김철수",15));
+
+		System.out.println(set1);
+		for(Student s : set1) {
+			System.out.println(s.hashCode());
+		}
 		
 	}
 }
--- lec11_collection/src/com/gn/study/model/vo/Student.java
+++ lec11_collection/src/com/gn/study/model/vo/Student.java
@@ -1,44 +1,8 @@
 package com.gn.study.model.vo;
 
-import java.util.Comparator;
+import java.util.Objects;
 
-public class Student implements Comparator<Student>{
-	private String name;
-	private int age;
-	
-	public Student() {}
-	
-	public Student(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
-	@Override
-	public String toString() {
-		return "[이름:"+name+",나이:"+ age+"]";
-	}
-	
-	
-	@Override
-	public boolean equals(Object obj) {
-		boolean result = false;
-		if(obj instanceof Student) {
-			Student s = (Student)obj;
-			if(s.name.equals(name) && s.age==age) {
-				result = true;
-			}
-		}
-		return result;
-	}
-
-	@Override
-	public int compare(Student o1, Student o2) {
-		if(o1.age > o2.age) return 1;
-		else if(o1.age < o2.age) return -1;
-		else return 0;
-	}
-}
-
-//public class Student implements Comparable<Student>{
+//public class Student implements Comparator<Student>{
 //	private String name;
 //	private int age;
 //	
@@ -55,16 +19,52 @@ public class Student implements Comparator<Student>{
 //	
 //	
 //	@Override
-//	public int compareTo(Student other) {
-//		// 자기자신의 age가 other의 age보다 크면 양수
-//		if(this.age > other.age) return 1;
-//		// 자기자신의 age가 other의 age보다 작으면 음수
-//		else if(this.age < other.age) return -1;
-//		// 두 값이 같으면 0
-//		else return 0;
-////		return this.age-other.age;		
+//	public boolean equals(Object obj) {
+//		boolean result = false;
+//		if(obj instanceof Student) {
+//			Student s = (Student)obj;
+//			if(s.name.equals(name) && s.age==age) {
+//				result = true;
+//			}
+//		}
+//		return result;
 //	}
-//	
+//
+//	@Override
+//	public int compare(Student o1, Student o2) {
+//		if(o1.age > o2.age) return 1;
+//		else if(o1.age < o2.age) return -1;
+//		else return 0;
+//	}
+//}
+
+public class Student implements Comparable<Student>{
+	private String name;
+	private int age;
+	
+	public Student() {}
+	
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "[이름:"+name+",나이:"+ age+"]";
+	}
+	
+	
+	@Override
+	public int compareTo(Student other) {
+		// 자기자신의 age가 other의 age보다 크면 양수
+		if(this.age > other.age) return 1;
+		// 자기자신의 age가 other의 age보다 작으면 음수
+		else if(this.age < other.age) return -1;
+		// 두 값이 같으면 0
+		else return 0;
+//		return this.age-other.age;		
+	}
+	
 //	@Override
 //	public boolean equals(Object obj) {
 //		boolean result = false;
@@ -76,4 +76,22 @@ public class Student implements Comparator<Student>{
 //		}
 //		return result;
 //	}
-//}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name,age);
+	}
+	
+//	@Override
+//	public boolean equals(Object obj) {
+//		boolean result = false;
+//		if(obj instanceof Student) {
+//			Student other = (Student)obj;
+//			if(other.name.equals(this.name) 
+//				&& other.age == this.age) {
+//				result = true;
+//			}
+//		}
+//		return result;
+//	}
+}
