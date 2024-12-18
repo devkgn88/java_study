d612ab6a04166a604ce726a5fc7398622430129c
devkgn88 <devkgn88@gmail.com>
2024-12-18 23:25:44 +0900

    [kgn] 파일 과제

---
+++ lec13_io/src/com/gn/homework03/Run.java
@@ -0,0 +1,9 @@
+package com.gn.homework03;
+
+import com.gn.homework03.view.FileMenu;
+
+public class Run {
+	public static void main(String[] args) {
+		new FileMenu().mainMenu();
+	}
+}
---
+++ lec13_io/src/com/gn/homework03/model/dao/FileDAO.java
@@ -0,0 +1,48 @@
+package com.gn.homework03.model.dao;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileDAO {
+	
+	public boolean checkName(String file) {
+		File f = new File(file);
+		boolean result = f.exists();
+		return result;
+	}
+
+	public void fileSave(String file, String s) {
+		try(FileWriter fw = new FileWriter(file);) {
+			fw.write(s);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public StringBuilder fileOpen(String file) {
+		StringBuilder read = new StringBuilder();
+		try(FileReader fr = new FileReader(file)){
+			int value;
+			while((value = fr.read()) != -1) {
+				read.append((char)value);
+			}
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return read;
+	}
+
+	public void fileEdit(String file, String s) {
+		try(FileWriter fw = new FileWriter(file, true);) {
+			fw.write(s);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}	
+}
