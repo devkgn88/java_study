dfc18f7d3f8483eab14a796aa7a7e85087080068
devkgn88 <devkgn88@gmail.com>
2024-12-19 21:32:39 +0900

    [kgn] 부동연산자

---
+++ lec13_io/src/com/gn/Run.java
@@ -0,0 +1,10 @@
+package com.gn;
+
+public class Run {
+	public static void main(String[] args) {
+		double a = 0.1;
+        double b = 0.2;
+        double sum = a + b;
+        System.out.println("0.1 + 0.2 = " + sum);
+	}
+}
